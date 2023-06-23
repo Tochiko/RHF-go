b7b5e6f766f528ec03eccfeda3861c9adac95bdd
RobinDBHW <thyeria@gmx.de>
2023-06-23 02:02:45 +0200

    correct S-Matrix now

--- chemical_system/atom.go
+++ chemical_system/atom.go
@@ -1,13 +1,18 @@
 package chemical_system
 
+const A0 = 0.529177210903 // 1 Bohr
+
 type Atom struct {
 	data  *AtomicData
 	coord [3]float64
 }
 
 func NewAtom(symbol string, coord [3]float64) *Atom {
-	return &Atom{
-		data:  PSE_BY_SYMBOL[symbol],
-		coord: coord,
+	result := &Atom{
+		data: PSE_BY_SYMBOL[symbol],
 	}
+	for i, c := range coord {
+		result.coord[i] = c / A0
+	}
+	return result
 }
--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	h1 := chemical_system.NewAtom("H", [3]float64{1.0, 0.0, 0.0})
 	h2 := chemical_system.NewAtom("H", [3]float64{0.0, 1.0, 0.0})
 	o1 := chemical_system.NewAtom("O", [3]float64{0.0, 0.0, 0.0})
-	h2o := chemical_system.NewMolecule([]*chemical_system.Atom{h1, h2, o1}, &chemical_system.STO3G)
+	h2o := chemical_system.NewMolecule([]*chemical_system.Atom{o1, h1, h2}, &chemical_system.STO3G)
 
 	S := h2o.GetS()
 	fmt.Print(mat.Formatted(S))
