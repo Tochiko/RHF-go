f90322fad302b36df3914b59a2362dd6154ed96f
RobinDBHW <thyeria@gmx.de>
2023-06-26 23:42:37 +0200

    added todo comment

--- basisfunctions/basisfunction.go
+++ basisfunctions/basisfunction.go
@@ -2,6 +2,7 @@ package basisfunctions
 
 import "RHF-go/chemical_system"
 
+// TODO: Replace the hard-coded Contracted3Gaussians in chemical system with BasisFunction interface
 type BasisFunction interface {
 	Normalize()
 	Copy() *BasisFunction
